16c5b1590d329bbd12d6a20e139d92ef5bf253eb
Paradox <maazkashif492@outlook.com>
2020-08-31 17:06:07 +0500

    Module done

---
+++ ParadoxUnboxingModule/ParadoxUnboxingModule/IconManager.cs
@@ -0,0 +1,41 @@
+﻿using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ParadoxUnboxingModule
+{
+    public class IconManager : MonoBehaviour
+    {
+        void Start()
+        {
+            DontDestroyOnLoad(this);
+            Player.onPlayerCreated += (p) => LoadItems();
+        }
+
+        public void LoadItems()
+        {
+            var assets = Assets.find(EAssetType.ITEM).Cast<ItemAsset>().ToList();
+
+            foreach (ItemAsset asset in assets)
+            {
+                var ready = new ItemIconReady((icon) =>
+                {
+                    AddUnturnedIcon(asset.id, icon.EncodeToPNG());
+                });
+                ItemTool.getIcon(asset.id, 0, asset.quality, asset.getState(), asset, null, string.Empty, string.Empty, asset.size_x * 250, asset.size_y * 250, false, true, ready);
+            }
+        }
+
+        public void AddUnturnedIcon(ushort id, byte[] icon)
+        {
+            if (File.Exists(Main.Instance.path + $"{id}.png"))
+                File.Delete(Main.Instance.path + $"{id}.png");
+            File.WriteAllBytes(Main.Instance.path + $"{id}.png", icon);
+        }
+    }
+}
---
+++ ParadoxUnboxingModule/ParadoxUnboxingModule/Main.cs
@@ -0,0 +1,35 @@
+﻿using SDG.Framework.Modules;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ParadoxUnboxingModule
+{
+    public class Main : IModuleNexus
+    {
+        public static GameObject gameObject;
+        public static Main Instance { get; set; }
+        public string path = Directory.GetCurrentDirectory() + "/Modules/ParadoxUnboxingModule/Icons/";
+
+        public void initialize()
+        {
+            Instance = this;
+
+            gameObject = new GameObject("ParadoxUnboxingModule");
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+            gameObject.AddComponent<IconManager>();
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+
+        public void shutdown()
+        {
+            UnityEngine.Object.Destroy(gameObject.GetComponent<IconManager>());
+        }
+    }
+}
